b1e9efac1764d600822ee593d7d8cd446a724bb3
Thiyagusrinivasan <thiyagusrinivasan10@gmail.com>
2024-08-01 21:28:54 +0530

    test-process

---
+++ pghostel/src/Content.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Content = () =>{
+var [item, setItem] = useState(
+  [
+    {
+      id:1,
+      checked: true,
+      item : "practices coding"
+    },
+  ]
+)
+  return (
+    <main>
+    
+    </main>
+  )
+  }
+
+export default Content
---
+++ pghostel/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+    var y= new Date();
+  return (
+    <footer>Copyright &copy; {y.getFullYear()}</footer>
+  )
+}
+
+export default Footer
